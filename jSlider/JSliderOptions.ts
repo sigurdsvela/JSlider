afd220aa4f20dd4a996294b069fe0dd0cb1a8768
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2014-01-16 14:57:57 +0100

    Change the default value for the "delay" option

--- jSlider/JSliderOptions.ts
+++ jSlider/JSliderOptions.ts
@@ -1,8 +1,8 @@
 module jSlider {
 	export class JSliderOptions {
 		private static _defaults = {
-			"delay" : 100, //Delay between each slide
-			"duration" : 100 //The duration of the slide animation
+			"delay" : 4000, //Delay between each slide
+			"duration" : 200 //The duration of the slide animation
 		};
 		
 		private options : Object<string, string> = {
