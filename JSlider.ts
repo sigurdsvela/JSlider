90e90d4b1bab07f53e14df18fcd22e856962ca2f
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2014-01-20 18:23:15 +0100

    Fix fractor sliding, sliding just a little bit, and enable this on touch
    *Add a getCanCycle in effect. And a can cycle in its consturctor
    parameter list
    *Add an init method in SLIDE:Effect
    *Implement FADE:Effect
    *gotoCurrent is back

--- JSlider.ts
+++ JSlider.ts
@@ -13,6 +13,17 @@ class JSlider {
 	private timeout : number;
 	private eventListeners : Array<Array<(jQuery) => void>>;
 	private effect : jSlider.Effect;
+
+	/**
+	 * Bounds of a single slide 
+	 */
+	private slideBounds : Object = {height:0,width:0};
+	
+	/*Touch Event*/
+	private startX : number = null;
+	private dx : number = null;
+	private lastX : number = null;
+	private fraction : number;
 	
 	/**
 	 * Creates a new slider out of an HTMLElement
@@ -44,10 +55,69 @@ class JSlider {
 		this.effect = <jSlider.Effect>this.options.get('effect');
 		
 		var _this = this;
+		
 		jQuery(function($) {
 			_this.sliderWrapper = jQuery(sliderWrapper);
 			_this.slidesWrapper = _this.sliderWrapper.children("ul").eq(0);
 			_this.slides = _this.slidesWrapper.children("li");
+			_this.slideBounds['height'] = _this.slides.eq(0).height();
+			_this.slideBounds['width'] = _this.slides.eq(0).width();
+
+			/**
+			 * Bind touch
+			 */
+			_this.slidesWrapper.on('touchmove', function(e) {
+				e.preventDefault();
+				var fraction = -((e.originalEvent.pageX - _this.startX) / _this.slideBounds['width']);
+				if (_this.startX != null) {
+					_this.fractionSlide(fraction);
+				}
+				
+				if (_this.lastX !== null) {
+					_this.dx = -(e.originalEvent.pageX - _this.lastX);
+				}
+				_this.lastX = e.originalEvent.pageX;
+			});
+			
+			_this.slidesWrapper.on('touchstart', function(e) {
+				_this.startX = e.originalEvent.pageX;
+			});
+
+			_this.slidesWrapper.on('touchend', function(e) {
+				_this.startX = null;
+				var absDelta = Math.abs(_this.dx);
+				
+				//Chose which slide we want to go to
+				if (absDelta < 4) { //Less than 4? treat as stationary
+					if (Math.abs(_this.fraction) < 0.5) {
+						_this.gotoCurrent();
+					} else {
+						if (_this.fraction > 0) {
+							_this.next();
+						} else {
+							_this.prev();
+						}
+					}
+				} else {
+					if (absDelta > 73) { //Delta overrides fraction
+						if (_this.dx > 0) {
+							_this.next();
+						} else {
+							_this.prev();
+						}
+					} else {
+						if ((_this.dx>0) != (_this.fraction>0)) { //If DX is not the same direction as fraction
+							_this.gotoCurrent();
+						} else {
+							if (_this.fraction > 0) {
+								_this.next();
+							} else {
+								_this.prev();
+							}
+						}
+					}
+				}
+			});
 			
 			_this.slides.css({
 				"position" : "relative",
@@ -66,6 +136,8 @@ class JSlider {
 				"display" : "block"
 			});
 			
+			_this.effect.init(_this.slidesWrapper, _this.slides, _this.currentSlide); //Initilize the effect
+			
 			var buttons = _this.options.get('button');
 			var button : any;
 			for (button in buttons) {
@@ -75,7 +147,7 @@ class JSlider {
 		});
 	}
 
-
+	
 
 	/**
 	 * Register a callback that should be called on <param>event</event>
@@ -149,25 +221,92 @@ class JSlider {
 	}
 
 	/**
-	 * Slide to the previous slide
+	 * Go back to the current slide
 	 */
-	public prev() : void {
-		var _prevSlide:number = this.currentSlide;
-		this.trigger('slide');
-		this.trigger('prev');
-		this.currentSlide = (this.currentSlide - 1 < 0) ? this.slides.length-1 : this.currentSlide - 1;
-		this.effect.gotoSlide(this.slidesWrapper, this.slides, _prevSlide, this.currentSlide, this.options.get('duration'));
+	private gotoCurrent() : void {
+		this.effect.gotoSlide(this.slidesWrapper, this.slides, this.currentSlide, this.currentSlide, this.options.get('duration'));
 	}
 
 	/**
-	 * Slide to the next slide
+	 * Whether or not we are at the last slide
+	 */
+	public atLastSlide() : boolean {
+		return (this.currentSlide >= this.slides.length-1);
+	}
+
+	/**
+	 * Whether ot not we are at the first slide
+	 */
+	public atFirstSlide() : boolean {
+		return (this.currentSlide === 0);
+	}
+
+	/**
+	 * Slide to the previous slide
+	 * If the effect can not cycle, then go to the current slide.
+	 */
+	public prev() : void {
+		var _prevSlide:number = this.currentSlide;
+		
+		var duration = this.options.get('duration');
+		duration = (duration-duration*Math.abs(this.fraction));
+		this.fraction = 0;
+		
+		if (!(!this.effect.getCanCycle() && this.atFirstSlide())) {
+			this.trigger('slide');
+			this.trigger('prev');
+			this.currentSlide = this.atFirstSlide() ? this.slides.length-1 : this.currentSlide - 1;
+		}
+		
+		this.effect.gotoSlide(this.slidesWrapper, this.slides, _prevSlide, this.currentSlide, duration);
+	}
+
+	/**
+	 * Slide to the next slide.
+	 * If the effect can not cycle, then go to the current slide.
 	 */
 	public next() : void {
 		var _prevSlide:number = this.currentSlide;
-		this.trigger('slide');
-		this.trigger('next');
-		this.currentSlide = (this.currentSlide + 1 > this.slides.length-1) ? 0 : this.currentSlide + 1;
-		this.effect.gotoSlide(this.slidesWrapper, this.slides, _prevSlide, this.currentSlide, this.options.get('duration'));
+
+		var duration = this.options.get('duration');
+		duration = (duration-duration*Math.abs(this.fraction));
+		this.fraction = 0;
+		
+		if (!(!this.effect.getCanCycle() && this.atLastSlide())) {
+			this.trigger('slide');
+			this.trigger('next');
+			this.currentSlide = this.atLastSlide() ? 0 : this.currentSlide + 1;
+		}
+		
+		this.effect.gotoSlide(this.slidesWrapper, this.slides, _prevSlide, this.currentSlide, duration);
+	}
+
+	/**
+	 * Slides a little bit to the next or previous slide
+	 * @param fraction A number between -1 and +1. + is next, - is previous
+	 */
+	private fractionSlide(fraction : number) : void {
+		var nextSlide : number;
+		
+		//If we cant cycle, but need to
+		
+		if (((this.atLastSlide() && fraction > 0) || (this.atFirstSlide() && fraction < 0)) && !this.effect.getCanCycle()) {
+			fraction/=2;
+			if (fraction > 0) {
+				nextSlide = this.currentSlide+1;
+			} else {
+				nextSlide = this.currentSlide-1;
+			}
+		} else {
+			if (fraction > 0) {
+				nextSlide = (this.currentSlide + 1 > this.slides.length-1) ? 0 : this.currentSlide + 1;
+			} else {
+				nextSlide = (this.currentSlide - 1 < 0) ? this.slides.length-1 : this.currentSlide - 1;
+			}
+		}
+		
+		this.fraction = fraction;
+		this.effect.fractionSlide(this.slidesWrapper, this.slides, this.currentSlide, nextSlide, fraction);
 	}
 
 	/**
